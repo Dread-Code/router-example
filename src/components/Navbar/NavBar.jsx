86568f9eb2c55e00d9ee5b4120e291abd3380ea4
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-10 00:35:16 -0500

    Navbar refactored

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,20 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
+/**
+ * NAVLINK is use for the activeClassName,
+ * this is functional because we could use
+ * some interactive class here.
+ *
+ */
 export const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        ReactRouter
+      </Link>
+      <div className="collapse navbar-collapse">
+        <div className="navbar-nav">
+          <NavLink
+            activeClassName="active"
+            exact
+            to="/"
+            className="nav-item nav-link"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            activeClassName="active"
+            exact
+            to="/about"
+            className="nav-item nav-link"
+          >
+            About
+          </NavLink>
+          <NavLink
+            activeClassName="active"
+            exact
+            to="/login"
+            className="nav-item nav-link"
+          >
+            Login
+          </NavLink>
+        </div>
+      </div>
     </nav>
   );
 };
