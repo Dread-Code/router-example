d37f44ae4bfe88843c8c88928268fb735465971b
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-10 00:46:35 -0500

    Context implemented

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import { RouterApp } from "./router/RouterApp";
 import { BrowserRouter as Router } from "react-router-dom";
 import { NavBar } from "./components/Navbar/NavBar";
+import { UserContext } from "./context/UserContext";
 
 export default function App() {
+  const user = {
+    id: 123,
+    name: "Lucas",
+    email: "lucasospina97@gmail.com",
+  };
   return (
-    <Router>
-      <NavBar />
-      <RouterApp />
-    </Router>
+    <UserContext.Provider value={user}>
+      <Router>
+        <NavBar />
+        <RouterApp />
+      </Router>
+    </UserContext.Provider>
   );
 }
